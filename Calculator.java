a3f60d1b03dba4f2c157b624d85251e0581009e7
samuel-adjin <samuel.adjin@amalitech.org>
2021-03-28 10:23:39 +0000

    first commit

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+/**
+ * Name: Samuel Adams Adjin
+ */
+
+
+
+public class Calculator {
+
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost(){
+        return floor.getArea() * carpet.getCost();
+    }
+}
+
